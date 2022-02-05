f4581fb7fb1bbde8b8bad7f3c02d9ff8a466febd
rascalmiles <rascal.miles@pcc.edu>
2022-02-05 01:21:05 -0800

    first commit

---
+++ README.txt
@@ -0,0 +1,26 @@
+Notes
+open a terminal (command + space)
+
+if running a project on a new computer, install node, install git, and run 
+
+sudo npm install -g http-server
+
+
+everytime we run 
+navigate to this project by typing 
+
+then run it with 
+http-server -c-1
+
+then point your browser at http://localhost:8080/
+right click - inspect in browser to see the command window
+
+while working on a project:
+open a terminal, navigate to your project
+
+do this if you added new files:
+git add .
+
+everytime you want to make changes to github:
+git commit -am'commit message goes here'
+git push
---
+++ sketch.js
@@ -0,0 +1,95 @@
+//defined variables at top are global scope
+let r = 96;
+let g = 81;
+let b = 196;
+
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  newColor = color(96, 81, 196);
+  background(0, 0, 0);
+  
+
+  // shapeGroup(0, 0, 0, 255, 0);
+  // shapeGroup(100, 100, r, g, b);
+  // shapeGroup(200, 200, 20, 60, 100);
+
+  moon(100, 100, r, g, b, 15);
+   fill(20);
+  ellipse(mouseX, mouseY, 300, 300);
+  // fill(r, g, b);
+  // ellipse(mouseX+50, mouseY, 300, 300);
+  // fill(20);
+  // ellipse(mouseX+100, mouseY, 300, 300);
+
+  // moon(100, 100, r, g, b, 15);
+  
+}
+
+//x y red green and blue are our function's parameters
+//these parameters only exist in this function and are not global
+//they can be passed to draw by calling them in the shapeGroup 
+//function in draw above
+function shapeGroup(x, y, red, green, blue) {
+  fill(red, green, blue);
+  ellipse(x+200, y+200, 200, 200);
+  ellipse(x+400, y+400, 400, 400);
+  rect(x+400, y+400, 100, 100);
+  print("shapeGroup is running");
+}
+
+function moon(x, y, red, green, blue, phase) {
+  //write code that will draw a moon in a different phase
+  //like 30 different phases or something
+  fill(red, green, blue); 
+
+  if ((phase >= 0) & (phase < 5)) {
+    ellipse(x+300, y+300, 300, 300);
+    fill(0);
+    ellipse((x+300)-((phase)*20), y+300, 300, 300);
+    }  
+  // if ((phase > 4) & (phase < 15)) {
+  //   ellipse(x+300, y+300, 300, 300);
+  //   fill(0);
+  //   ellipse((x+300)-((phase)*20), y+300, 300, (300+(phase*15)));
+  //   }  
+  if ((phase > 4) & (phase < 8)) {
+    ellipse(x+300, y+300, 300, 300);
+    fill(0);
+    ellipse((((x+300) + (phase * 10))-((phase)*20)), y+300, (300-(phase*20)), (300+(phase*20)));
+    }  
+    if (phase === 8) {
+    ellipse(x+300, y+300, 300, 300);
+    fill(0);
+    rect(x+150, y+150, 150, 300);
+    }
+    if ((phase > 8) & (phase < 15)) {
+    ellipse(x+300, y+300, 300, 300);
+    fill(0);
+    ellipse((((x+300) + (phase * 10))-((phase)*20)), y+300, (300-(phase*20)), (300+(phase*20)));
+    }  
+  if ((phase > 15) & (phase < 21)) {
+    ellipse(x+300, y+300, 300, 300);
+    fill(0);
+    ellipse((x+600)-((phase-15)*20), y+300, 300, (300+(phase*15)));  
+  }
+  if (phase === 21) {
+    ellipse(x+300, y+300, 300, 300);
+    fill(0);
+    rect(x+300, y+150, 150, 300);
+    }
+  if ((phase > 21) & (phase <= 30)) {
+    ellipse(x+300, y+300, 300, 300);
+    fill(0);
+    ellipse((x+600)-((phase-15)*20), y+300, 300, 300);  
+  }
+  if (phase === 15) {
+    b = 0;
+    ellipse(x+300, y+300, 300, 300);
+    fill(0);
+    ellipse((x+300)-((phase)*20), y+300, 300, 300);
+  }
+  print("moon is running");
+}
